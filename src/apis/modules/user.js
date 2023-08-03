d1e72dd84e9ddd24172a3a38c689b305f793625b
Mieu Teacher <mieuteacher@gmail.com>
2023-08-03 18:26:47 +0700

    token

--- src/apis/modules/user.js
+++ src/apis/modules/user.js
@@ -4,6 +4,7 @@ const router = express.Router()
 
 import userController from '../../controllers/user.controller';
 
+router.get('/confirm/:token', userController.confirm)
 router.get('/', userController.read)
 router.post('/', userController.create)
 
